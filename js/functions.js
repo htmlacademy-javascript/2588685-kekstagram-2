456f91ad59c8192b300c81f88178461fd1c60885
averoslyn <averoslyn@gmail.com>
2025-09-06 19:12:35 +0300

    Добавляет вторую функцию

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,11 @@ function validateStringLength(string ='', length = 1) {
   return string.length <= length;
 }
 
+function isPalidrome (string = '') {
+  string = string.replaceAll(' ','').toLowerCase();
+  let reversedString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string.at(i);
+  }
+  return string === reversedString;
+}
