186f78ebd636f318c7b2f3d324faa438892912b6
averoslyn <averoslyn@gmail.com>
2025-09-07 19:26:13 +0300

    Вносит правки на основе созвонов

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,18 @@
+//Функция 1. Проверка длины строки
+
 function validateStringLength(string = '', length = 1) {
   return string.length <= length;
 }
 
-validateStringLength ('проверяемая строка', 10);
+window.console.log(validateStringLength('проверяемая строка', 20));
+window.console.log(validateStringLength('проверяемая строка', 18));
+window.console.log(validateStringLength('проверяемая строка', 10));
+
+
+//Функция 2. Проверка палидрома
 
 function isPalidrome (string = '') {
-  string = string.replaceAll(' ','').toLowerCase();
+  string = string.toString().replaceAll(' ', '').toLowerCase();
   let reversedString = '';
   for (let i = string.length - 1; i >= 0; i--) {
     reversedString += string.at(i);
@@ -13,4 +20,33 @@ function isPalidrome (string = '') {
   return string === reversedString;
 }
 
-isPalidrome('Лёша на полке клопа нашёл ');
+window.console.log(isPalidrome('топот'));
+window.console.log(isPalidrome('ДовОд'));
+window.console.log(isPalidrome('Кекс'));
+window.console.log(isPalidrome('Лёша на полке клопа нашёл '));
+
+
+//Функция 3. Выбор чисел из строки
+
+function extractNumbers(input) {
+  input = input.toString();
+  let result = '';
+
+  for (let i = 0; i < input.length; i++) {
+    let char = input[i];
+    if (char >= '0' && char <= '9') {
+      result += char;
+    }
+  }
+
+  return parseInt(result, 10);
+}
+
+window.console.log(extractNumbers('2023 год'));
+window.console.log(extractNumbers('ECMAScript 2022'));
+window.console.log(extractNumbers('1 кефир, 0.5 батона'));
+window.console.log(extractNumbers('агент 007'));
+window.console.log(extractNumbers('а я томат'));
+window.console.log(extractNumbers(2023));
+window.console.log(extractNumbers(-1));
+window.console.log(extractNumbers(1.5));
