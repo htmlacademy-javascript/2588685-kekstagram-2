14b59c227216f476eda676154ce78b4e384556b1
averoslyn <averoslyn@gmail.com>
2025-09-14 01:03:10 +0300

    Вносит правки на основе созвонов

--- js/render-big-pictures.js
+++ js/render-big-pictures.js
@@ -5,28 +5,31 @@ const socialCommentsContainer = bigPictureContainer.querySelector('.social__comm
 const socialCommentTemplate = bigPictureContainer.querySelector('.social__comment'); //Шаблон с комментариями
 const socialCommentCount = bigPictureContainer.querySelector('.social__comment-count'); //Блок счётчика
 const commentsLoader = bigPictureContainer.querySelector('.comments-loader'); //Блок для загрузки комментариев
-const commentShownCountText = bigPictureContainer.querySelector('.social__comment-shown-count').textContent; //Количество показанных комментариев
+//const commentShownCountText = bigPictureContainer.querySelector('.social__comment-shown-count').textContent; Количество показанных комментариев
 const bigPictureCancel = bigPictureContainer.querySelector('.big-picture__cancel');
 
 const onBigPictureCancelClick = () => {
   closeBigPicture();
 };
 
-const onEscKeydown = (evt) => {
+const onDocumentEscKeydown = (evt) => {
   if (evt.key === 'Escape' || evt.key === 'Esc') {
     closeBigPicture();
   }
 };
 
-const closeBigPicture = () => {
+function closeBigPicture() {
 
   bigPictureContainer.classList.add('hidden');
 
   document.body.classList.remove('modal-open');
 
   bigPictureCancel.removeEventListener('click', onBigPictureCancelClick);
-  document.removeEventListener('keydown', onEscKeydown);
-};
+  document.removeEventListener('keydown', onDocumentEscKeydown);
+}
+
+socialCommentCount.classList.add('hidden');
+commentsLoader.classList.add('hidden');
 
 function openBigPicture(pictureId) {
 
@@ -51,27 +54,15 @@ function openBigPicture(pictureId) {
   });
 
   socialCommentsContainer.append(commentsFragment);
-  bigPictureContainer.querySelector('.social__caption').textContent = currentPhoto.description;
 
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  bigPictureContainer.querySelector('.social__caption').textContent = currentPhoto.description;
 
   document.body.classList.add('modal-open');
   bigPictureContainer.classList.remove('hidden');
 
   bigPictureCancel.addEventListener('click', onBigPictureCancelClick);
-  document.addEventListener('keydown', onEscKeydown);
-};
+  document.addEventListener('keydown', onDocumentEscKeydown);
+}
 
 
 export { openBigPicture };
-/*
-При закрытии окна (нажатии на клавиши esc или по иконке закрытия):
-у bigPictureContainer добавляется класс hidden
-у body убирается класс modal-open
-*/
-/*
-После открытия окна:
-добавьте тегу <body> класс modal-open, чтобы контейнер с фотографиями позади не прокручивался при скролле.
-Напишите код для закрытия окна по нажатию клавиши Esc и клике по иконке закрытия.
-Подключите модуль в проект.*/
