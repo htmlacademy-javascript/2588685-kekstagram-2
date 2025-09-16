95b7bb9ef044c4eaea18e0e8ae93b6b12124edf9
Keks <keks@htmlacademy.ru>
2025-09-16 15:45:38 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,16 @@
+import { photos } from './generate-photos.js';
+import { openBigPicture } from './render-big-pictures.js';
+import { picturesContainer, renderPictures } from './render-thumbnails.js';
+import './validate-form.js';
+import './edit-picture.js';
+
+renderPictures(photos);
+
+picturesContainer.addEventListener('click', (evt) => {
+  const currentPicture = evt.target.closest('.picture');
+
+  if (currentPicture) {
+    evt.preventDefault();
+    openBigPicture(currentPicture.dataset.id);
+  }
+});
